6f10d1bf7743a95026b12be1b2a09b9f7fab3123
vventura <vincent.ventura1@gmail.com>
2017-07-05 13:36:43 +0200

    modification des textures de fond

--- Assets/Scripts/Controllers/GameController.cs
+++ Assets/Scripts/Controllers/GameController.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class GameController : MonoBehaviour {
 
+    public GameObject constructionCameraContainer;
 	public Camera constructionCamera;
 	public BlockService blockS;
 	public ConstructionGuiController constructionGui;
@@ -12,6 +13,7 @@ public class GameController : MonoBehaviour {
 	private Ray mouseRay;
 	private RaycastHit cellHit;
 	private CellController selectedCell;
+    private PlayerController playerController;
 
 	// Use this for initialization
 	void Awake () {
@@ -20,20 +22,13 @@ public class GameController : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
+        // Mouse controls
 		if (phase == GamePhase.CONSTRUCTION) {
 			if (Input.GetMouseButton (0)) {
 				selectedCell = null;
 				GetSelectedCell ();
 
-				if (selectedCell != null) {
-					if (blockS.CanPlaceCurrentBlock ()) {
-						selectedCell.SetBlock (blockS.CurrentBlock);
-
-						if (blockS.CurrentBlock.CompareTag ("PlayerSpawn")) {
-							blockS.SpawnPlaced = true;
-						}
-					}
-				}
+                DoTriggerAction(selectedCell);
 			} else if (Input.GetMouseButton (1)) {
 				selectedCell = null;
 				GetSelectedCell ();
@@ -49,10 +44,39 @@ public class GameController : MonoBehaviour {
 		}
 	}
 
-	public void LaunchGame () {
-		phase = GamePhase.PLAY;
+    public void DoTriggerAction (CellController cell)
+    {
+        if (phase == GamePhase.CONSTRUCTION)
+        {
+            selectedCell = cell;
+
+            if (selectedCell != null)
+            {
+                if (blockS.CanPlaceCurrentBlock())
+                {
+                    selectedCell.SetBlock(blockS.CurrentBlock);
+
+                    if (blockS.CurrentBlock.CompareTag("PlayerSpawn"))
+                    {
+                        blockS.SpawnPlaced = true;
+                    }
+                }
+            }
+        }
+    }
+
+    public void DoTriggerAction(Vector3 direction)
+    {
+        if (phase == GamePhase.PLAY)
+        {
+            playerController.TriggeredMoveHandler(direction);
+        }
+    }
+
+    public void LaunchGame () {
 		CellController[] cells = FindObjectsOfType<CellController> ();
 		constructionGui.HideConstructionElement ();
+        constructionCameraContainer.SetActive(false);
 
 		foreach (CellController cell in cells) {
 			cell.HidePlaceholder ();
@@ -63,9 +87,10 @@ public class GameController : MonoBehaviour {
 			dis.Disappear ();
 		}
 
-		FindObjectOfType<PlayerSpawnController> ().SpawnPlayer ();
+		playerController = FindObjectOfType<PlayerSpawnController> ().SpawnPlayer ();
 		constructionCamera.gameObject.SetActive (false);
-	}
+        phase = GamePhase.PLAY;
+    }
 
 	private void GetSelectedCell () {
 		mouseRay = constructionCamera.ScreenPointToRay (Input.mousePosition);
@@ -74,6 +99,14 @@ public class GameController : MonoBehaviour {
 			selectedCell = cellHit.collider.GetComponent<CellController> ();
 		}
 	}
+
+    public GamePhase Phase
+    {
+        get
+        {
+            return phase;
+        }
+    }
 
 	public enum GamePhase {
 		CONSTRUCTION, 
---
+++ Assets/Scripts/Controllers/VR/VRController.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VRController : MonoBehaviour {
+
+    private SteamVR_TrackedController controller;
+    private GameController gameC;
+    private RaycastHit hit; 
+
+    // Use this for initialization
+    void Start () {
+        controller = GetComponent<SteamVR_TrackedController>();
+        gameC = FindObjectOfType<GameController>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (controller.triggerPressed) {
+            if (gameC.Phase == GameController.GamePhase.CONSTRUCTION)
+            {
+                if (Physics.Raycast(transform.position, transform.forward, out hit)) {
+                    // Check hit object (ui or cell)
+                    VRButtonInteractionController buttonController = hit.collider.GetComponent<VRButtonInteractionController>();
+                    CellController cell = hit.collider.GetComponent<CellController>();
+                    VRPlayButtonController playButtonController = hit.collider.GetComponent<VRPlayButtonController>();
+
+                    if (buttonController != null)
+                    {
+                        buttonController.TriggeredHandler();
+                    } else if (cell != null)  {
+                        gameC.DoTriggerAction(cell);
+                    } else if (playButtonController != null) {
+                        playButtonController.TriggeredHandler();
+                    }
+                }
+            } else {
+                gameC.DoTriggerAction(transform.forward);
+            }
+        } else if (controller.padPressed && gameC.Phase == GameController.GamePhase.CONSTRUCTION) {
+            RaycastHit[] hits = Physics.RaycastAll(transform.position, transform.forward);
+
+            foreach (RaycastHit hitInfo in hits)
+            {
+                CellController cell = hitInfo.collider.GetComponent<CellController>();
+
+                if (cell != null)
+                {
+                    cell.EmptyCell();
+                }
+            }
+        }
+    }
+}
